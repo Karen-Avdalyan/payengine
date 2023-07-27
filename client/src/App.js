80e873cd8b702b79ae06c6162a8fc04be4503b96
Karen-Avdalyan <karenavdalian@gmail.com>
2023-07-27 22:15:27 +0400

    minor fix

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@
 import { Layout } from './Components/Layout';
 import { AuthProvider } from './Contexts/Auth';
 import { Router } from './Router';
-import './App.css';
 
 function App() {
   return (
